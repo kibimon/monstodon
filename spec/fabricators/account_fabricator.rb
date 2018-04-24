e92813291990c489018249540fbfb087e8982ef3
Surinna Curtis <ekiru.0@gmail.com>
2018-04-23 21:10:10 -0400

    Assign trainer_no's when fabricating accounts.

--- spec/fabricators/account_fabricator.rb
+++ spec/fabricators/account_fabricator.rb
@@ -1,5 +1,6 @@
 Fabricator(:account) do
   type 'ActivityMon::Trainer'
+  trainer_no { sequence(:trainer_no, 1) }
   username { sequence(:username) { |i| "#{Faker::Internet.user_name(nil, %w(_))}#{i}" } }
   last_webfingered_at { Time.now.utc }
 end
