3c06a8afa6039a33720265378d882bdced3fec27
Surinna Curtis <ekiru.0@gmail.com>
2018-05-05 14:38:11 -0500

    Start numero sequences in fabricators at 1.

--- spec/fabricators/activitymon_route_fabricator.rb
+++ spec/fabricators/activitymon_route_fabricator.rb
@@ -1,7 +1,7 @@
 Fabricator('ActivityMon::Route', aliases: %i(route v2_route)) do
   routing_version 2
   last_webfingered_at { Time.now.utc }
-  route_regional_no { sequence(:route_regional_no) }
-  route_national_no { sequence(:route_national_no) }
+  route_regional_no { sequence(:route_regional_no, 1) }
+  route_national_no { sequence(:route_national_no, 1) }
 
 end
